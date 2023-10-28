d4b0eafb44cd7ce36cd04ff88bbb7edf50a3da3b
Bruno Oliveira <bruno1_oliveira@hotmail.com>
2023-10-28 20:07:43 -0300

    :sparkles: feat: criado rotas iniciais do site

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 // Components
 import Banner from './components/Banner'
 import Header from './components/Header'
-
 // Styles
 import { GlobalStyles } from './global/styles'
+
+const rotas = createBrowserRouter([{ path: '/', element: <Banner /> }])
 
 function App() {
   return (
@@ -12,7 +14,7 @@ function App() {
       <div className="container">
         <Header />
       </div>
-      <Banner />
+      <RouterProvider router={rotas} />
     </>
   )
 }
