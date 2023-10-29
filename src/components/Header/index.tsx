2ee22a20ee5c1863fc4b48a0278c9e4940d8f81e
Bruno Oliveira <bruno1_oliveira@hotmail.com>
2023-10-29 11:32:39 -0300

    :sparkles: feat: adicionado link para rota principal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,7 +7,9 @@ import cart from '../../assets/images/carrinho.svg'
 const Header = () => (
   <HeaderBar>
     <div>
-      <img src={logo} alt="EPLAY" />
+      <Link to="/">
+        <img src={logo} alt="EPLAY" />
+      </Link>
       <nav>
         <Links>
           <LinkItem>
