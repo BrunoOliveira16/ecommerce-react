1d0da859186d530cead00345980fdbdc2547a6f8
Bruno Oliveira <bruno1_oliveira@hotmail.com>
2023-10-29 10:56:29 -0300

    :sparkles: feat: criado pagina Home

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,104 @@
+import Banner from '../../components/Banner'
+import ProductsList from '../../components/ProductsList'
+import Game from '../../models/Game'
+
+import Resident from '../../assets/images/resident.png'
+import Diablo from '../../assets/images/diablo.png'
+import Zelda from '../../assets/images/zelda.png'
+import StarWars from '../../assets/images/star_wars.png'
+
+const promocoes: Game[] = [
+  {
+    id: 1,
+    category: 'Ação',
+    description:
+      'Resident Evil 4, conhecido no Japão como Biohazard 4, é um jogo eletrônico de survival horror...',
+    title: 'Resident Evil 4',
+    system: 'Windows',
+    infos: ['10%', 'R$ 250,00'],
+    image: Resident
+  },
+  {
+    id: 2,
+    category: 'Ação',
+    description:
+      'Resident Evil 4, conhecido no Japão como Biohazard 4, é um jogo eletrônico de survival horror...',
+    title: 'Resident Evil 4',
+    system: 'PS5',
+    infos: ['5%', 'R$ 290,00'],
+    image: Resident
+  },
+  {
+    id: 3,
+    category: 'Ação',
+    description:
+      'Resident Evil 4, conhecido no Japão como Biohazard 4, é um jogo eletrônico de survival horror...',
+    title: 'Resident Evil 4',
+    system: 'Windows',
+    infos: ['10%', 'R$ 250,00'],
+    image: Resident
+  },
+  {
+    id: 4,
+    category: 'Ação',
+    description:
+      'Resident Evil 4, conhecido no Japão como Biohazard 4, é um jogo eletrônico de survival horror...',
+    title: 'Resident Evil 4',
+    system: 'Windows',
+    infos: ['10%', 'R$ 250,00'],
+    image: Resident
+  }
+]
+
+const emBreve: Game[] = [
+  {
+    id: 5,
+    category: 'RPG',
+    description:
+      'Diablo IV é um RPG de ação em desenvolvimento pela Blizzard Entertainment.',
+    title: 'Diablo 4',
+    image: Diablo,
+    system: 'Windows',
+    infos: ['05/04']
+  },
+  {
+    id: 6,
+    category: 'Aventura',
+    description:
+      'Star Wars Jedi: Survivor é um próximo jogo de ação e aventura desenvolvido pela Respawn...',
+    title: 'Star wars Jedi Survivor',
+    image: StarWars,
+    system: 'Windows',
+    infos: ['05/04']
+  },
+  {
+    id: 7,
+    category: 'RPG',
+    description:
+      'Uma aventura épica pela terra e pelos céus de Hyrule aguarda em The Legend of Zelda...',
+    title: 'The Legend of Zelda - TOK',
+    image: Zelda,
+    system: 'Switch',
+    infos: ['05/04']
+  },
+  {
+    id: 8,
+    category: 'RPG',
+    description:
+      'Diablo IV é um RPG de ação em desenvolvimento pela Blizzard Entertainment.',
+    title: 'Diablo 4',
+    image: Diablo,
+    system: 'Windows',
+    infos: ['05/04']
+  }
+]
+
+const Home = () => (
+  <>
+    <Banner />
+    <ProductsList games={promocoes} title="Promoções" background="gray" />
+    <ProductsList games={emBreve} title="Em Breve" background="black" />
+  </>
+)
+
+export default Home
