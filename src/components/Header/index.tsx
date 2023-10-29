884a204ccf703f6a99aaeb236f1ea06fec7c47ec
Bruno Oliveira <bruno1_oliveira@hotmail.com>
2023-10-29 10:57:16 -0300

    :sparkles: feat: adicionado Link para rota de categorias

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom'
+
 import { HeaderBar, LinkCart, LinkItem, Links } from './styles'
 import logo from '../../assets/images/logo.svg'
 import cart from '../../assets/images/carrinho.svg'
@@ -9,7 +11,7 @@ const Header = () => (
       <nav>
         <Links>
           <LinkItem>
-            <a href="#">Categorias</a>
+            <Link to="/categories">Categorias</Link>
           </LinkItem>
           <LinkItem>
             <a href="#">Novidades</a>
