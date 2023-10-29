9bb788d5ae32762deda15bef9023cc208475dfbb
Bruno Oliveira <bruno1_oliveira@hotmail.com>
2023-10-29 09:51:10 -0300

    :sparkles: feat: criado props para Product e adicionado no corpo do componente

--- src/components/Product/index.tsx
+++ src/components/Product/index.tsx
@@ -1,19 +1,35 @@
 import Tag from '../Tag'
-import { Card, Title, Description } from './styles'
+import { Card, Title, Description, Infos } from './styles'
 
-const Product = () => {
+type Props = {
+  title: string
+  category: string
+  system: string
+  description: string
+  infos: string[]
+  image: string
+}
+
+const Product = ({
+  title,
+  category,
+  system,
+  description,
+  infos,
+  image
+}: Props) => {
   return (
     <Card>
-      <img src="//placehold.it/222x250" alt="card" />
-      <Title>Nome do Jogo</Title>
-      <Tag>Categoria</Tag>
-      <Tag>Windows</Tag>
-      <Description>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
-        consequatur ea quaerat, nam odio distinctio totam voluptatibus rem, sed
-        aliquam adipisci porro laudantium commodi, ipsam omnis repellendus
-        veritatis! Quo, dolores!
-      </Description>
+      <img src={image} alt={title} />
+      <Infos>
+        {infos.map((info) => (
+          <Tag key={info}>{info}</Tag>
+        ))}
+      </Infos>
+      <Title>{title}</Title>
+      <Tag>{category}</Tag>
+      <Tag>{system}</Tag>
+      <Description>{description}</Description>
     </Card>
   )
 }
--- src/components/Product/styles.ts
+++ src/components/Product/styles.ts
@@ -6,6 +6,7 @@ export const Card = styled.div`
   background-color: ${Colors.gray};
   border-radius: 8px;
   padding: 8px;
+  position: relative;
 
   ${TagContainer} {
     margin-right: 8px;
@@ -25,4 +26,10 @@ export const Description = styled.p`
   line-height: 22px;
   display: block;
   margin-top: 16px;
+`
+
+export const Infos = styled.div`
+  position: absolute;
+  top: 16px;
+  right: 16px;
 `
