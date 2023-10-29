f34388a0bd9487c36bff30e1d4f4789ba0fe67b2
Bruno Oliveira <bruno1_oliveira@hotmail.com>
2023-10-29 09:52:01 -0300

    :sparkles: feat: criado logica para estilizar o background do card e fundo da section

--- src/components/ProductsList/index.tsx
+++ src/components/ProductsList/index.tsx
@@ -1,20 +1,48 @@
 import Product from '../Product'
 import { Container, List, Title } from './styles'
 
-type Props = {
+export type Props = {
   title: string
   background: 'gray' | 'black'
 }
 
-const ProductsList = ({ title }: Props) => (
-  <Container>
+const ProductsList = ({ background, title }: Props) => (
+  <Container background={background}>
     <div className="container">
       <Title>{title}</Title>
       <List>
-        <Product />
-        <Product />
-        <Product />
-        <Product />
+        <Product
+          category="Ação"
+          description="teste"
+          image="//placehold.it/222x250"
+          infos={['-10%', 'R$ 150']}
+          system="Windows"
+          title="Nome do Jogo"
+        />
+        <Product
+          category="Ação"
+          description="teste"
+          image="//placehold.it/222x250"
+          infos={['-10%', 'R$ 150']}
+          system="Windows"
+          title="Nome do Jogo"
+        />
+        <Product
+          category="Ação"
+          description="teste"
+          image="//placehold.it/222x250"
+          infos={['-10%', 'R$ 150']}
+          system="Windows"
+          title="Nome do Jogo"
+        />
+        <Product
+          category="Ação"
+          description="teste"
+          image="//placehold.it/222x250"
+          infos={['-10%', 'R$ 150']}
+          system="Windows"
+          title="Nome do Jogo"
+        />
       </List>
     </div>
   </Container>
--- src/components/ProductsList/styles.ts
+++ src/components/ProductsList/styles.ts
@@ -1,7 +1,17 @@
 import styled from 'styled-components'
+import { Props } from '.'
+import { Colors } from '../../global/styles'
+import { Card } from '../Product/styles'
 
-export const Container = styled.section`
+export const Container = styled.section<Omit<Props, 'title'>>`
   padding: 32px 0;
+  background-color: ${(props) =>
+    props.background === 'black' ? Colors.black : Colors.gray};
+
+  ${Card} {
+    background-color: ${(props) =>
+      props.background === 'black' ? Colors.gray : Colors.black};
+  }
 `
 
 export const List = styled.ul`
