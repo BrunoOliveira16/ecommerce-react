e3b4d19f698e83e0c8d975de136543eb6dc81f74
Bruno Oliveira <bruno1_oliveira@hotmail.com>
2023-10-29 09:23:08 -0300

    :sparkles: feat: importado em App o componente ProductsList

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,21 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 // Components
 import Banner from './components/Banner'
 import Header from './components/Header'
+import ProductsList from './components/ProductsList'
 // Styles
 import { GlobalStyles } from './global/styles'
 
-const rotas = createBrowserRouter([{ path: '/', element: <Banner /> }])
+const rotas = createBrowserRouter([
+  {
+    path: '/',
+    element: (
+      <>
+        <Banner />
+        <ProductsList title="Promoções" background="gray" />
+      </>
+    )
+  }
+])
 
 function App() {
   return (
