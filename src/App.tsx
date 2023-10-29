b60c74040369689c7bc47471a46c413971aea1c8
Bruno Oliveira <bruno1_oliveira@hotmail.com>
2023-10-29 11:33:23 -0300

    :sparkles: feat: adicionado Footer ao App

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter } from 'react-router-dom'
 import Rotas from './routes/routes'
 
 import Header from './components/Header'
+import Footer from './components/Footer'
 import { GlobalStyles } from './global/styles'
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Header />
       </div>
       <Rotas />
+      <Footer />
     </BrowserRouter>
   )
 }
