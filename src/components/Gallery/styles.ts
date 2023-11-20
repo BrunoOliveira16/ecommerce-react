f38892cebd17d9c053cf89024fe939b761f6023b
Bruno Oliveira <bruno1_oliveira@hotmail.com>
2023-11-20 18:26:55 -0300

    :sparkles: feat: criado componentes estilizados para modal e action

--- src/components/Gallery/styles.ts
+++ src/components/Gallery/styles.ts
@@ -6,12 +6,84 @@ export const Itens = styled.ul`
   gap: 16px;
 `
 
+export const Action = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.73);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  opacity: 0;
+  transition: opacity 0.5s ease;
+`
+
 export const Item = styled.li`
-  img {
+  position: relative;
+
+  > img {
     border: 2px solid ${Colors.white};
     border-radius: 8px;
     width: 150px;
     height: 150px;
     object-fit: cover;
+  }
+
+  &:hover {
+    ${Action} {
+      opacity: 1;
+      transition: opacity 0.5s ease;
+    }
+  }
+`
+
+export const Modal = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  .overlay {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.73);
+  }
+`
+
+export const ModalContent = styled.div`
+  max-width: 960px;
+  position: relative;
+  z-index: 1;
+
+  header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-bottom: 24px;
+
+    h4 {
+      font-size: 18px;
+      font-weight: bold;
+    }
+
+    img {
+      width: 16px;
+      height: 16px;
+    }
+  }
+
+  img {
+    display: block;
+    max-width: 100%;
   }
 `
