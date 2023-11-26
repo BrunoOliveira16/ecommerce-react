df08ce679a5b09a4cd0176545c35baab11101daf
Bruno Oliveira <bruno1_oliveira@hotmail.com>
2023-11-26 12:36:31 -0300

    :sparkles: feat: adicionado chamada de api ao componente para exibição dinâmica

--- src/pages/Categories/index.tsx
+++ src/pages/Categories/index.tsx
@@ -1,104 +1,49 @@
+import { useEffect, useState } from 'react'
 import ProductsList from '../../components/ProductsList'
-import Game from '../../models/Game'
+import { GameProps } from '../Home'
 
-import Resident from '../../assets/images/resident.png'
-import Diablo from '../../assets/images/diablo.png'
-import Zelda from '../../assets/images/zelda.png'
-import StarWars from '../../assets/images/star_wars.png'
+const Categories = () => {
+  const [rpgGames, setRpgGames] = useState<GameProps[]>([])
+  const [fightGames, setFightGames] = useState<GameProps[]>([])
+  const [sportsGames, setSportsGames] = useState<GameProps[]>([])
+  const [actionGames, setActionGames] = useState<GameProps[]>([])
+  const [simulationGames, setSimulationGames] = useState<GameProps[]>([])
 
-const promocoes: Game[] = [
-  {
-    id: 1,
-    category: 'Ação',
-    description:
-      'Resident Evil 4, conhecido no Japão como Biohazard 4, é um jogo eletrônico de survival horror...',
-    title: 'Resident Evil 4',
-    system: 'Windows',
-    infos: ['10%', 'R$ 250,00'],
-    image: Resident
-  },
-  {
-    id: 2,
-    category: 'Ação',
-    description:
-      'Resident Evil 4, conhecido no Japão como Biohazard 4, é um jogo eletrônico de survival horror...',
-    title: 'Resident Evil 4',
-    system: 'PS5',
-    infos: ['5%', 'R$ 290,00'],
-    image: Resident
-  },
-  {
-    id: 3,
-    category: 'Ação',
-    description:
-      'Resident Evil 4, conhecido no Japão como Biohazard 4, é um jogo eletrônico de survival horror...',
-    title: 'Resident Evil 4',
-    system: 'Windows',
-    infos: ['10%', 'R$ 250,00'],
-    image: Resident
-  },
-  {
-    id: 4,
-    category: 'Ação',
-    description:
-      'Resident Evil 4, conhecido no Japão como Biohazard 4, é um jogo eletrônico de survival horror...',
-    title: 'Resident Evil 4',
-    system: 'Windows',
-    infos: ['10%', 'R$ 250,00'],
-    image: Resident
-  }
-]
+  useEffect(() => {
+    fetch('https://fake-api-tau.vercel.app/api/eplay/rpg')
+      .then((res) => res.json())
+      .then((res) => setRpgGames(res))
 
-const emBreve: Game[] = [
-  {
-    id: 5,
-    category: 'RPG',
-    description:
-      'Diablo IV é um RPG de ação em desenvolvimento pela Blizzard Entertainment.',
-    title: 'Diablo 4',
-    image: Diablo,
-    system: 'Windows',
-    infos: ['05/04']
-  },
-  {
-    id: 6,
-    category: 'Aventura',
-    description:
-      'Star Wars Jedi: Survivor é um próximo jogo de ação e aventura desenvolvido pela Respawn...',
-    title: 'Star wars Jedi Survivor',
-    image: StarWars,
-    system: 'Windows',
-    infos: ['05/04']
-  },
-  {
-    id: 7,
-    category: 'RPG',
-    description:
-      'Uma aventura épica pela terra e pelos céus de Hyrule aguarda em The Legend of Zelda...',
-    title: 'The Legend of Zelda - TOK',
-    image: Zelda,
-    system: 'Switch',
-    infos: ['05/04']
-  },
-  {
-    id: 8,
-    category: 'RPG',
-    description:
-      'Diablo IV é um RPG de ação em desenvolvimento pela Blizzard Entertainment.',
-    title: 'Diablo 4',
-    image: Diablo,
-    system: 'Windows',
-    infos: ['05/04']
-  }
-]
+    fetch('https://fake-api-tau.vercel.app/api/eplay/luta')
+      .then((res) => res.json())
+      .then((res) => setFightGames(res))
 
-const Categories = () => (
-  <>
-    <ProductsList games={promocoes} title="RPG" background="gray" />
-    <ProductsList games={emBreve} title="Ação" background="black" />
-    <ProductsList games={promocoes} title="Aventura" background="gray" />
-    <ProductsList games={emBreve} title="FPS" background="black" />
-  </>
-)
+    fetch('https://fake-api-tau.vercel.app/api/eplay/esportes')
+      .then((res) => res.json())
+      .then((res) => setSportsGames(res))
+
+    fetch('https://fake-api-tau.vercel.app/api/eplay/acao')
+      .then((res) => res.json())
+      .then((res) => setActionGames(res))
+
+    fetch('https://fake-api-tau.vercel.app/api/eplay/simulacao')
+      .then((res) => res.json())
+      .then((res) => setSimulationGames(res))
+  }, [])
+
+  return (
+    <>
+      <ProductsList games={actionGames} title="Ação" background="black" />
+      <ProductsList games={sportsGames} title="Esporte" background="gray" />
+      <ProductsList games={fightGames} title="Luta" background="black" />
+      <ProductsList games={rpgGames} title="RPG" background="gray" />
+      <ProductsList
+        games={simulationGames}
+        title="Simulação"
+        background="black"
+      />
+    </>
+  )
+}
 
 export default Categories
