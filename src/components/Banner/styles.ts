57b6bd4b8471823519c976116488b8078cd9b4bc
Bruno Oliveira <bruno1_oliveira@hotmail.com>
2023-11-26 12:32:48 -0300

    :sparkles: feat: adicionado chamada de api ao componente para exibição dinâmica

--- src/components/Banner/styles.ts
+++ src/components/Banner/styles.ts
@@ -8,6 +8,7 @@ export const Image = styled.div`
   background-repeat: no-repeat;
   background-size: cover;
   font-weight: bold;
+  position: relative;
 
   .container {
     position: relative;
@@ -15,11 +16,22 @@ export const Image = styled.div`
     display: flex;
     justify-content: space-between;
     align-items: flex-end;
+    z-index: 1;
   }
 
   ${TagContainer} {
     position: absolute;
     top: 32px;
+  }
+
+  &::after {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    content: '';
   }
 `
 
