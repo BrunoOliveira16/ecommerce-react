7bbcba19babd1d8218843335f56919f6d93b67cd
Bruno Oliveira <bruno1_oliveira@hotmail.com>
2023-11-26 12:35:48 -0300

    :sparkles: feat: melhoria na exibição das tags

--- src/components/ProductsList/index.tsx
+++ src/components/ProductsList/index.tsx
@@ -1,32 +1,53 @@
-import Game from '../../models/Game'
 import Product from '../Product'
+import { GameProps } from '../../pages/Home'
+import { formatPrice } from '../../utils/functions'
 import { Container, List, Title } from './styles'
 
 export type Props = {
   title: string
   background: 'gray' | 'black'
-  games: Game[]
+  games: GameProps[]
 }
 
-const ProductsList = ({ background, title, games }: Props) => (
-  <Container background={background}>
-    <div className="container">
-      <Title>{title}</Title>
-      <List>
-        {games.map((game) => (
-          <Product
-            key={game.id}
-            category={game.category}
-            description={game.description}
-            image={game.image}
-            infos={game.infos}
-            system={game.system}
-            title={game.title}
-          />
-        ))}
-      </List>
-    </div>
-  </Container>
-)
+const ProductsList = ({ background, title, games }: Props) => {
+  const getGameTags = (game: GameProps) => {
+    const tags = []
+
+    if (game.release_date) {
+      tags.push(game.release_date)
+    }
+
+    if (game.prices?.discount) {
+      tags.push(`${game.prices.discount}%`)
+    }
+
+    if (game.prices?.current) {
+      tags.push(formatPrice(game.prices.current))
+    }
+
+    return tags
+  }
+
+  return (
+    <Container background={background}>
+      <div className="container">
+        <Title>{title}</Title>
+        <List>
+          {games.map((game) => (
+            <Product
+              key={game.id}
+              category={game.details.category}
+              description={game.description}
+              image={game.media.thumbnail}
+              infos={getGameTags(game)}
+              system={game.details.system}
+              title={game.name}
+            />
+          ))}
+        </List>
+      </div>
+    </Container>
+  )
+}
 
 export default ProductsList
