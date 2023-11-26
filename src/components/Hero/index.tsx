8c827fab8d29fcea113bb1574d28a299e502ecbd
Bruno Oliveira <bruno1_oliveira@hotmail.com>
2023-11-26 18:31:44 -0300

    :sparkles: feat: adicionado props game e exibição dinâmica das informações

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,31 +1,44 @@
-import bannerimg from '../../assets/images/fundo_hogwarts.png'
+import { GameProps } from '../../pages/Home'
+import { getPrices } from '../../utils/functions'
 import Button from '../Button'
 import Tag from '../Tag'
 import { Banner, Infos } from './styles'
 
-const Hero = () => (
-  <Banner style={{ backgroundImage: `url(${bannerimg})` }}>
-    <div className="container">
-      <div>
-        <Tag>RPG</Tag>
-        <Tag>PS5</Tag>
+type GameHeroProps = {
+  game: GameProps
+}
+
+const Hero = ({ game }: GameHeroProps) => {
+  return (
+    <Banner style={{ backgroundImage: `url(${game.media.cover})` }}>
+      <div className="container">
+        <div>
+          <Tag>{game.details.category}</Tag>
+          <Tag>{game.details.system}</Tag>
+        </div>
+        <Infos>
+          <h2>{game.name}</h2>
+          {game.prices?.current && (
+            <>
+              <p>
+                {game.prices?.discount && (
+                  <span>De {getPrices(game.prices?.old)}</span>
+                )}
+                Por {getPrices(game.prices?.current)}
+              </p>
+              <Button
+                variant="primary"
+                type="button"
+                title="Clique aqui para adicionar o jogo ao carrinho"
+              >
+                Adicionar ao carrinho
+              </Button>
+            </>
+          )}
+        </Infos>
       </div>
-      <Infos>
-        <h2>Hogwarts Legacy</h2>
-        <p>
-          <span>De R$ 250,00</span>
-          Por R$ 190,00
-        </p>
-        <Button
-          variant="primary"
-          type="button"
-          title="Clique aqui para adicionar o jogo ao carrinho"
-        >
-          Adicionar ao carrinho
-        </Button>
-      </Infos>
-    </div>
-  </Banner>
-)
+    </Banner>
+  )
+}
 
 export default Hero
