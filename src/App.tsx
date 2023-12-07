836a84ce1005d8d5d548a7391b8235a2b3260665
Bruno Oliveira <bruno1_oliveira@hotmail.com>
2023-12-07 20:42:10 -0300

    feat: import de Provider do redux em App

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,26 @@
+import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
+
 import Rotas from './routes/routes'
+import { store } from './store'
 
 import Header from './components/Header'
 import Footer from './components/Footer'
+
 import { GlobalStyles } from './global/styles'
 
 function App() {
   return (
-    <BrowserRouter>
-      <GlobalStyles />
-      <div className="container">
-        <Header />
-      </div>
-      <Rotas />
-      <Footer />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <GlobalStyles />
+        <div className="container">
+          <Header />
+        </div>
+        <Rotas />
+        <Footer />
+      </BrowserRouter>
+    </Provider>
   )
 }
 
