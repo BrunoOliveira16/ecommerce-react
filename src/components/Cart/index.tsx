34f850d50a0de1ed0fd9a794b36b055479675488
Bruno Oliveira <bruno1_oliveira@hotmail.com>
2023-12-09 16:12:11 -0300

    feat: criado componente Cart

---
+++ src/components/Cart/index.tsx
@@ -0,0 +1,50 @@
+import Button from '../Button'
+import {
+  Overlay,
+  CartContainer,
+  Sidebar,
+  Prices,
+  Quantity,
+  CartItem
+} from './styles'
+import cartImage from '../../assets/images/star_wars.png'
+import Tag from '../Tag'
+
+const Cart = () => (
+  <CartContainer>
+    <Overlay />
+    <Sidebar>
+      <ul>
+        <CartItem>
+          <img src={cartImage} alt="imagem" />
+          <div>
+            <h3>Nome do jogo</h3>
+            <Tag>RPG</Tag>
+            <Tag>PS5</Tag>
+            <span>R$ 150,00</span>
+          </div>
+          <button type="button" />
+        </CartItem>
+        <CartItem>
+          <img src={cartImage} alt="imagem" />
+          <div>
+            <h3>Nome do jogo</h3>
+            <Tag>RPG</Tag>
+            <Tag>PS5</Tag>
+            <span>R$ 150,00</span>
+          </div>
+          <button type="button" />
+        </CartItem>
+      </ul>
+      <Quantity>2 jogo(s) no carrinho</Quantity>
+      <Prices>
+        Total de R% 250,00 <span>Em até 6x juros</span>
+      </Prices>
+      <Button title="Clique aqui para continuar com a compra" type="button">
+        Continuar com a compra
+      </Button>
+    </Sidebar>
+  </CartContainer>
+)
+
+export default Cart
---
+++ src/components/Cart/styles.ts
@@ -0,0 +1,101 @@
+import styled from 'styled-components'
+import { Colors } from '../../global/styles'
+import { TagContainer } from '../Tag/styles'
+import { ButtonContainer } from '../Button/styles'
+import Close from '../../assets/images/close.png'
+
+export const CartContainer = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: flex-end;
+  z-index: 1;
+`
+
+export const Overlay = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: #000;
+  opacity: 0.7;
+`
+
+export const Sidebar = styled.aside`
+  max-width: 360px;
+  width: 100%;
+  padding: 40px 16px 0 16px;
+  background-color: ${Colors.gray};
+  z-index: 1;
+
+  ${ButtonContainer} {
+    max-width: 100%;
+    width: 100%;
+  }
+`
+
+export const Prices = styled.p`
+  font-size: 14px;
+  font-weight: bold;
+  color: ${Colors.white};
+  margin-bottom: 24px;
+
+  span {
+    display: block;
+    font-size: 12px;
+    color: ${Colors.lightGray};
+  }
+`
+
+export const Quantity = styled.p`
+  font-size: 16px;
+  font-weight: bold;
+  color: ${Colors.white};
+  margin: 32px 0 16px;
+`
+
+export const CartItem = styled.li`
+  display: flex;
+  border-bottom: 1px solid ${Colors.lightGray};
+  padding: 8px 0;
+  position: relative;
+
+  img {
+    width: 80px;
+    height: 80px;
+    object-fit: cover;
+    margin-right: 24px;
+  }
+
+  h3 {
+    font-size: 16px;
+    font-weight: bold;
+    color: ${Colors.white};
+  }
+
+  span {
+    display: block;
+    font-size: 14px;
+    font-weight: bold;
+    color: ${Colors.white};
+  }
+
+  ${TagContainer} {
+    margin: 8px 8px 16px 0;
+  }
+
+  button {
+    background-image: url(${Close});
+    width: 16px;
+    height: 16px;
+    border: none;
+    background-color: transparent;
+    position: absolute;
+    top: 8px;
+    right: 0;
+  }
+`
