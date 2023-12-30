56faa3807a61eeac66bb0b3f9f43432d5f8217a8
Bruno Oliveira <bruno1_oliveira@hotmail.com>
2023-12-30 10:33:46 -0300

    style: melhorias na responsividade do formulario

--- src/pages/Checkout/styles.ts
+++ src/pages/Checkout/styles.ts
@@ -1,5 +1,5 @@
 import styled from 'styled-components'
-import { Colors } from '../../global/styles'
+import { Breakpoints, Colors } from '../../global/styles'
 
 type InputGroupProps = {
   maxWidth?: string
@@ -18,6 +18,11 @@ export const Row = styled.div<RowProps>`
   align-items: flex-end;
   gap: 24px;
   margin-top: ${(props) => props.marginTop || '0'};
+
+  @media (max-width: ${Breakpoints.tablet}) {
+    display: block;
+    margin-top: 16px;
+  }
 `
 
 export const InputGroup = styled.div<InputGroupProps>`
@@ -42,6 +47,10 @@ export const InputGroup = styled.div<InputGroupProps>`
       border: 1px solid red;
     }
   }
+
+  @media (max-width: ${Breakpoints.tablet}) {
+    margin-top: 16px;
+  }
 `
 
 export const TabButton = styled.button<TabButtonProps>`
@@ -61,5 +70,10 @@ export const TabButton = styled.button<TabButtonProps>`
     width: 16px;
     height: 13px;
     margin-right: 8px;
+  }
+
+  @media (max-width: ${Breakpoints.tablet}) {
+    margin-top: 8px;
+    width: 100%;
   }
 `
